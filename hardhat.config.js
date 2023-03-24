9af9347da54149a48da82de3f9915d2daadc8924
Saurabh Lodha <saurabhlodha221b@gmail.com>
2023-03-24 19:13:30 +0530

    Hardhat Init and Forked Mainnet Setup

---
+++ hardhat.config.js
@@ -0,0 +1,25 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    version: "0.8.0",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    localhost: {
+      url: "http://localhost:8545",
+    },
+    hardhat: {
+      forking: {
+        url: process.env.INFURA_RPC_URL,
+      },
+    },
+  },
+};
